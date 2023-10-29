b497b05b722b468e88e7bd108301d39884bc491a
Tigers <morpheuss99@gmail.com>
2023-10-29 04:39:33 +0100

    Az EntityController osztály teszt lefedettsége 100%

--- src/test/java/hu/nye/pandragon/wumpus/service/game/EntityControllerTest.java
+++ src/test/java/hu/nye/pandragon/wumpus/service/game/EntityControllerTest.java
@@ -1,7 +1,9 @@
 package hu.nye.pandragon.wumpus.service.game;
 
 import hu.nye.pandragon.wumpus.model.Directions;
+import hu.nye.pandragon.wumpus.model.TurnDirections;
 import hu.nye.pandragon.wumpus.model.entities.Hero;
+import hu.nye.pandragon.wumpus.model.entities.Pit;
 import hu.nye.pandragon.wumpus.model.entities.Wall;
 import hu.nye.pandragon.wumpus.model.entities.Wumpus;
 import org.junit.jupiter.api.BeforeEach;
@@ -23,9 +25,38 @@ class EntityControllerTest {
 	}
 
 	@Test
-	public void shouldMoveForward () {
-		level.placeEntity(2, 3, hero);
+	public void shouldMoveForwardNorth () {
+		level.placeEntity(3, 3, hero);
 		assertTrue(controller.moveForward());
+	}
+
+	@Test
+	public void shouldMoveForwardEast () {
+		level.placeEntity(3, 3, hero);
+		hero.setDirection(Directions.East);
+		assertTrue(controller.moveForward());
+	}
+
+	@Test
+	public void shouldMoveForwardSouth () {
+		level.placeEntity(3, 3, hero);
+		hero.setDirection(Directions.South);
+		assertTrue(controller.moveForward());
+	}
+
+	@Test
+	public void shouldMoveForwardWest () {
+		level.placeEntity(3, 3, hero);
+		hero.setDirection(Directions.West);
+		assertTrue(controller.moveForward());
+	}
+
+	@Test
+	public void shouldActionOnHeroEnters () {
+		level.placeEntity(3, 3, hero);
+		level.placeEntity(3, 2, new Pit());
+		controller.moveForward();
+		assertEquals(hero.getAmmoAmount(), 0);
 	}
 
 	@Test
@@ -40,6 +71,20 @@ class EntityControllerTest {
 		controller.turnRight();
 		var result = hero.getDirection();
 		assertEquals(Directions.East, result);
+	}
+
+	@Test
+	public void shouldTurnRight2 () {
+		level.placeEntity(2, 2, hero);
+		controller.turn(TurnDirections.Right);
+		assertEquals(hero.getDirection(), Directions.East);
+	}
+
+	@Test
+	public void shouldTurnLeft2 () {
+		level.placeEntity(2, 2, hero);
+		controller.turn(TurnDirections.Left);
+		assertEquals(hero.getDirection(), Directions.West);
 	}
 
 	@Test
@@ -74,4 +119,5 @@ class EntityControllerTest {
 		controller.shoot();
 		assertEquals(level.getFirstEntityInDirection(hero.getPosition(), hero.getDirection()).getClass(), Wall.class);
 	}
+
 }
